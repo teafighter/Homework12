eae7607c90bb4143e95a4de2e201cca8aa81f141
Kirill Tyurikov <teafighter@gmail.com>
2024-03-18 14:39:51 +0300

    Проверь плиз

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        
+        Author tolstoy = new Author("Leo", "Tolstoy");
+        Author dostoevsky = new Author("Fedor", "Dostoevsky");
+        Book warAndPeace = new Book("War and peace", tolstoy, 1867);
+        Book crimeAndPunishment = new Book(" Crime and Punishment", dostoevsky, 1866);
+        crimeAndPunishment.setYear(1867);
         }
     }
