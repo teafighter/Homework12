c7b9d92d5fad5cfde0dd5c746b1bf2882cd32c5d
Kirill Tyurikov <teafighter@gmail.com>
2024-03-20 15:45:19 +0300

    overridden toString(), equals() and hashCode()

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private final String title;
     private final Author author;
@@ -23,5 +25,23 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book name is " + title + ", its author " + author + " wrote it in " + year + " year";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
     }
 }
